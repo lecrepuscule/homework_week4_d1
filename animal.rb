bd0d926f9bcd28aaf9fdae73c712298280cb3603
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-31 18:39:06 +0100

    added animal class

--- animal.rb
+++ animal.rb
@@ -1,6 +1,13 @@
 class Animal
 
-  def initialize
+  attr_accessor :name, :breed, :age, :gender, :favourite_toys
+  
+  def initialize (animal_data)
+    @name = animal_data[:name]
+    @breed = animal_data[:breed]
+    @age = animal_data[:age]
+    @gender = animal_data[:gender]
+    @favourite_toys = animal_data[:favourite_toys]
   end
 
 end
--- client.rb
+++ client.rb
@@ -1,10 +1,12 @@
 class Client
 
+  attr_accessor :name, :age, :gender, :number_of_pats
+
   def initialize (personal_data)
-    @personal_data[:name] = personal_data[:name]
-    @personal_data[:age] = personal_data[:age]
-    @personal_data[:gender] = personal_data[:gender]
-    @personal_data[:number_of_pats] = personal_data[:number_of_pats]
+    @name = personal_data[:name]
+    @age = personal_data[:age]
+    @gender = personal_data[:gender]
+    @number_of_pats = personal_data[:number_of_pats]
   end
 
   def adopt_animal
